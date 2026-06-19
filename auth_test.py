8e30bd3c1a35dd28e25c70538599a3fdb52a67e1
Veesheen Yuen <veeyuen@gmail.com>
2026-06-19 15:51:00 +0800

    Add auth test app

---
+++ auth_test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Auth Test", layout="centered")
+
+st.title("Auth Test")
+
+if not st.user.is_logged_in:
+    st.info("Not logged in yet.")
+    st.button("Log in with Google", on_click=st.login)
+    st.stop()
+
+st.success("Logged in successfully.")
+st.write("Email:", st.user.get("email", ""))
+st.write("Name:", st.user.get("name", ""))
+
+st.button("Log out", on_click=st.logout)
